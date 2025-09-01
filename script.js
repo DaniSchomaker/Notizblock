a7625eddbb97229352c73ff4371c5c1f078669a2
Dani S. <daniela.schomaker@posteo.de>
2025-09-01 16:40:26 +0200

    added LocalStorage (first steps)

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Notizen anzeigen lassen
 
 // ich brauche Notizen
-let notesTitles = ['Ba','Aufgabe'];
-let notes = ['banana', 'rasen mähen'];
+let notesTitles = ["Ba", "Aufgabe"];
+let notes = ["banana", "rasen mähen"];
 
 let trashNotesTitles = [];
 let trashNotes = [];
@@ -23,11 +23,10 @@ function renderNotes() {
 }
 
 //  // Das HTML wird aus der function renderNotes() ausgegliedert
-function getNoteTemplate(indexNote) {   // get = kriegen wir // Übergabeparameter (note)
+function getNoteTemplate(indexNote) {
+  // get = kriegen wir // Übergabeparameter (note)
   return `<p>+ title: ${notesTitles[indexNote]} -> ${notes[indexNote]}<button onclick="noteToTrash(${indexNote})">X</button></p>`;
 }
-
-
 
 // Papierkorb anlegen
 function renderTrashNotes() {
@@ -36,7 +35,7 @@ function renderTrashNotes() {
 
   for (
     let indexTrashNote = 0;
-    indexTrashNote < trashNotes.length; 
+    indexTrashNote < trashNotes.length;
     indexTrashNote++ ////
   ) {
     trashContentRef.innerHTML += getTrashNoteTemplate(indexTrashNote); ////
@@ -47,11 +46,6 @@ function renderTrashNotes() {
 function getTrashNoteTemplate(indexTrashNote) {
   return `<p>+ title: ${trashNotesTitles[indexTrashNote]} ---> ${trashNotes[indexTrashNote]}<button onclick="deleteNote(${indexTrashNote})">X</button></p>`;
 }
-
-
-
-
-
 
 // Notizen hinzufügen
 
@@ -66,26 +60,52 @@ function addNote() {
   renderNotes(); // Eingabe anzeigen lassen
 
   noteInputRef.value = ""; //am Ende das Eingabefeld leeren
-}
 
+  saveToLocalStorage(); // Wenn eine Notiz hinzugefügt wurde, wird diese im LocalStorage gespeichert
+}
 
 // Notizen dem Papierkorb hinzufügen bzw. ganz löschen & Anzeige updaten
 
 function noteToTrash(indexNote) {
-    let trashNote = notes.splice(indexNote, 1); // EINZELNE (!) trashNote ungleich trashNotes!
-    trashNotes.push(trashNote[0]);
-    let trashNoteTitle = notesTitles.splice(indexNote, 1);
-    trashNotesTitles.push(trashNoteTitle[0]);
-    renderNotes();
-    renderTrashNotes();
-}
+  let trashNote = notes.splice(indexNote, 1); // EINZELNE (!) trashNote ungleich trashNotes!
+  trashNotes.push(trashNote[0]);
+  let trashNoteTitle = notesTitles.splice(indexNote, 1);
+  trashNotesTitles.push(trashNoteTitle[0]);
+  renderNotes();
+  renderTrashNotes();
 
+  saveToLocalStorage(); // Wenn eine Notiz in den Papierkorb geschoben wird, wird diese im LocalStorage gespeichert
+}
 
 function deleteNote(indexTrashNote) {
   trashNotes.splice(indexTrashNote, 1);
+  trashNotesTitles.splice(indexTrashNote, 1); // hinzugefügt, damit auch der Titel gelöscht wird
   renderNotes();
-  renderTrashNotes(); 
+  renderTrashNotes();
+
+  saveToLocalStorage(); // Wenn eine Notiz endgültig gelöscht wird, wird das neue Array im LocalStorage gespeichert
 }
+
+// Aufgabe 10: Implementiere nun in deinem Notizblock, dass alles im LocalStorage
+// gespeichert wird und beim Neuladen der Seite auch angezeigt wird.
+
+// WAS? Im LocalStorage müssen folgende Arrays gespeichert werden:
+// notes, notesTitles, trashNotes, trashNotesTitles
+
+// WANN? Immer, wenn sich etwas ändert (hinzufügen, verschieben, löschen etc.)
+
+// ACHTUNG: LocalStorage kann keine Arrays speichern, nur Strings 
+// --> Beim Speichern: JSON.stringify (in String umwandeln)
+// --> Beim Laden: JSON.parse (von String wieder in Array umwandeln)
+
+function saveToLocalStorage() {
+  // localStorage.setItem("myData", JSON.stringify(myData)); //Vorlage
+  localStorage.setItem("notes", JSON.stringify(notes));
+  localStorage.setItem("notesTitles", JSON.stringify(notesTitles));
+  localStorage.setItem("trashNotes", JSON.stringify(trashNotes));
+  localStorage.setItem("trashNotesTitles", JSON.stringify(trashNotesTitles));
+}
+
 
 
 
